c20d01c62deef9cfb8c4700644e15bc288594e47
Nijat Rzayev <rzaev.nicat@gmail.com>
2023-05-25 16:45:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def root():
     return {"message": "Hello World. Welcome to FastAPI!"}
 
 
-@app.get("/path")
+@app.get("/nijat")
 async def demo_get():
     return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
 
